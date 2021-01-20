fb99ac5496c65c87c3345bfc4c583b50ecbdd660
bhowell2 <bhowell2@users.noreply.github.com>
2021-01-20 20:08:06 +0000

    Updated distribution materials.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,44 +1,211 @@
 export interface TimeoutOptions {
+    /**
+     * Number of milliseconds from creation time to run the Timeout.
+     */
     timeMillis: number;
+    /**
+     * Whether to indefinitely reschedule on the same interval once
+     * the Timeout expires. This can be cancelled with {@link Timeout#cancel}
+     * or overridden with adding another Timeout of the same key (and
+     * bucket, of course) and setting {@link overwriteKey} to true.
+     */
     recurring?: boolean;
+    /**
+     * Unique, identifying, key for the Timeout. Used to avoid creating
+     * multiple timers of the same key and also provides debounce type
+     * functionality.
+     *
+     * No key means that the callback is treated independently, even if
+     * it has strict equality with another another callback that has already
+     * been scheduled.
+     */
     key?: string;
+    /**
+     * Adds the key to a bucket so that all keys in the bucket can be
+     * treated together.
+     *
+     * Defaults to {@link Scheduler#DEFAULT_BUCKET_KEY}.
+     */
     bucketKey?: string;
+    /**
+     * Whether or not to overwrite the Timeout if the key already exists.
+     *
+     * This also acts as a debouncer. If overwriteKey = true this will
+     * cause the timeout to debounce until no other calls have been made
+     * to add a Timeout with the same key before it has been called. If
+     * overwriteKey = false then only the first callback for the key will
+     * be called within the interval.
+     *
+     * Defaults to false.
+     */
     overwriteKey?: boolean;
+    /**
+     * When paused, the time remaining can either be saved and started back
+     * when the Timeout is resumed or it can use the Timeout's original expiration.
+     * When this is true, the original expiration will be used; when this is false
+     * the expiration will be adjusted by the pause time.
+     *
+     * E.g., Timeout has 250ms before expiration and is paused; resume is called
+     * after 500ms; if ignorePauseDelay = true then the Timeout's callback would
+     * be run immediately, however if ignorePauseDelay = false then the Timeout
+     * will run 250ms after resume is called.
+     *
+     * Note this can be overridden by passing in ignorePauseDelay in the pause or
+     * resume of the Timeout or Scheduler.
+     *
+     * Defaults to false (i.e., adjust expiration by pause delay).
+     */
     ignorePauseDelay?: boolean;
 }
+/**
+ * Return value from creating a Timeout. Allows for checking the state
+ * of the Timeout and pausing, resuming, or canceling the Timeout. The
+ * options used to create the Timeout are also provided here along with
+ * any defaults that were set for them.
+ */
 export interface Timeout {
+    /**
+     * Pretty self-explanatory. Note that the state will never be 'complete'
+     * if the Timeout is recurring.
+     */
     readonly state: "pending" | "paused" | "complete";
+    /**
+     * Whether or not the Timeout has completed running. This will always be
+     * false for recurring Timeouts. (This is just state === "complete".)
+     */
     readonly isComplete: boolean;
+    /**
+     * Allows for pausing the Timeout for any amount of time. When resumed
+     * one of two things will happen:
+     * 1. TimeoutOptions.ignorePauseDelay = false: the time remaining on the
+     *    callback will be used to generate the new expiration time from
+     *    that moment forward.
+     * 2. TimeoutOptions.ignorePauseDelay = true: the expiration time will not
+     *    change when resumed and will expire (callback called) whenever it
+     *    originally would have expired.
+     *
+     * This allows for providing ignorePauseDelay on a per-call basis, which will
+     * override the existing TimeoutOptions.ignorePauseDelay if provided. Leave
+     * this as undefined if it is not desired to override TimeoutOptions.ignorePauseDelay.
+     */
     readonly pause: (ignorePauseDelay?: boolean) => void;
+    /**
+     * Starts the Timeout back if it was paused, otherwise does nothing.
+     * See {@link pause} for more information.
+     *
+     * This allows for providing ignorePauseDelay on a per-call basis, which will
+     * override the existing TimeoutOptions.ignorePauseDelay if provided. Leave
+     * this as undefined if it is not desired to override TimeoutOptions.ignorePauseDelay.
+     */
     readonly resume: (ignorePauseDelay?: boolean) => void;
+    /**
+     * Cancels the Timeout from running if it has not already run or is recurring.
+     */
     readonly cancel: () => void;
+    /**
+     * How long until the Timeout is called.
+     */
     readonly remainingTime: number;
+    /**
+     * The bucketKey and key will have been set to defaults on this if they were
+     * not originally provided.
+     */
     readonly options: Readonly<TimeoutOptions>;
 }
+/**
+ * The Scheduler must be provided an interval to check for events. This interval
+ * can be as fine as 1 (or even 0...) millisecond. This allows the user to create
+ * multiple Schedulers with tighter or looser intervals. By default the Scheduler
+ * will be started when it is created, but this can be overridden by providing
+ * false as the second constructor argument.
+ *
+ * This is not exported as a default as it is a bad practice IM(and others)O.
+ */
 export declare class Scheduler {
     static DEFAULT_BUCKET_KEY: string;
     private idCounter;
     private readonly interval;
     private expectedSchedulerIntervalExpiration;
     private remainingPauseTime;
+    /**
+     * Used to track the timeouts for each bucket. Timeouts that do not
+     * specify a bucket are stored here with DEFAULT_BUCKET_KEY.
+     */
     private buckets;
     private shutdown;
     private timeoutId;
     constructor(intervalMillis: number, start?: boolean);
     private started;
+    /**
+     * Starts the scheduler if it has not already been started. Can
+     * be used to start again after it has been stopped.
+     */
     start: () => void;
+    /**
+     * Stops the scheduler.
+     */
     stop: () => void;
     private __INTERNAL__runExpiredCallbacks;
     private __INTERNAL__run;
+    /**
+     * Pauses all jobs and the scheduler itself.
+     *
+     * Optionally supply ignorePauseDelay to override each Timeout's
+     * ignorePauseDelay setting.
+     */
     pause: (ignorePauseDelay?: boolean) => void;
+    /**
+     * Resumes the scheduler and all paused timeouts. This does not resume
+     * all paused timeouts if the scheduler itself is not paused. If this
+     * is desired, use {@link getTimeoutsForBucket} and loop through them.
+     *
+     * Optionally supply ignorePauseDelay to override each Timeout's
+     * ignorePauseDelay setting.
+     */
     resume: (ignorePauseDelay?: boolean) => void;
+    /**
+     * Adds the callback to be run with the provided options. Defaults will be
+     * set if they are not supplied. options must be a number that will correspond
+     * to a time (in milliseconds) or options must be an object containing 'timeMillis'.
+     * The created Timeout is returned so that it can be controlled as desired.
+     */
     add: (callback: () => void, options: number | TimeoutOptions) => Timeout;
+    /**
+     * Pauses the Timeout if it exists. Returns the Timeout that was paused or
+     * undefined if the Timeout did not exist.
+     */
     pauseTimeout: (timeoutKey: string, bucketKey?: string, ignorePauseDelay?: boolean) => Timeout | undefined;
+    /**
+     * Pauses the Timeout if it exists. Returns the Timeout that was paused or
+     * undefined if the Timeout did not exist.
+     */
     pauseTimeoutOnDefaultBucket: (timeoutKey: string, ignorePauseDelay?: boolean) => Timeout | undefined;
+    /**
+     * Resumes the Timeout if it exists. Returns the Timeout that was resumed or
+     * undefined if the Timeout did not exist.
+     */
     resumeTimeout: (timeoutKey: string, bucketKey?: string, ignorePauseDelay?: boolean) => Timeout | undefined;
+    /**
+     * Resumes the Timeout if it exists. Returns the Timeout that was resumed or
+     * undefined if the Timeout did not exist.
+     */
     resumeTimeoutOnDefaultBucket: (timeoutKey: string, ignorePauseDelay?: boolean) => Timeout | undefined;
+    /**
+     * Returns all timeouts that have not expired on the given bucket.
+     */
     getTimeoutsForBucket: (bucketKey: string) => Timeout[];
+    /**
+     * Returns all timeouts that have not expired on the default bucket.
+     */
     getDefaultBucketTimeouts: () => Timeout[];
+    /**
+     * Returns the timeout of the given key on the given bucket if it exists
+     * or undefined if it does not exist.
+     */
     getTimeoutForKey: (key: string, bucketKey?: string) => Timeout | undefined;
+    /**
+     * Returns the timeout of the given key on the default bucket if it exists
+     * or undefined if it does not exist.
+     */
     getTimeoutForDefaultBucket: (key: string) => Timeout | undefined;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,51 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Scheduler = void 0;
-var Scheduler = (function () {
+/**
+ * The Scheduler must be provided an interval to check for events. This interval
+ * can be as fine as 1 (or even 0...) millisecond. This allows the user to create
+ * multiple Schedulers with tighter or looser intervals. By default the Scheduler
+ * will be started when it is created, but this can be overridden by providing
+ * false as the second constructor argument.
+ *
+ * This is not exported as a default as it is a bad practice IM(and others)O.
+ */
+var Scheduler = /** @class */ (function () {
     function Scheduler(intervalMillis, start) {
         var _a;
         var _this = this;
         if (start === void 0) { start = true; }
         this.idCounter = 0;
+        /**
+         * Used to track the timeouts for each bucket. Timeouts that do not
+         * specify a bucket are stored here with DEFAULT_BUCKET_KEY.
+         */
         this.buckets = (_a = {},
             _a[Scheduler.DEFAULT_BUCKET_KEY] = {},
             _a);
         this.shutdown = false;
         this.started = false;
+        /**
+         * Starts the scheduler if it has not already been started. Can
+         * be used to start again after it has been stopped.
+         */
         this.start = function () {
-            if (!_this.started) {
+            if (!_this.started) { // ensures multiple
                 _this.shutdown = false;
                 _this.started = true;
                 _this.__INTERNAL__run(_this.interval);
             }
         };
+        /**
+         * Stops the scheduler.
+         */
         this.stop = function () {
+            // need to keep track of this so that
             if (!_this.shutdown) {
                 _this.shutdown = true;
                 _this.started = false;
                 clearTimeout(_this.timeoutId);
+                // clear all Timeouts
                 _this.buckets = {};
             }
         };
@@ -32,13 +54,15 @@ var Scheduler = (function () {
                 var bucket = _this.buckets[bucketsKey];
                 for (var bucketKey in bucket) {
                     var timeout = bucket[bucketKey];
-                    if (timeout.state === "pending" && timeout.remainingTime <= 0) {
+                    if (timeout.state === "pending" && timeout.remainingTime <= 0) { // will call
+                        // run it and reset the expiration or remove it
                         timeout.callback();
                         if (timeout.options.recurring) {
                             timeout.expiration = Date.now() + timeout.options.timeMillis;
                         }
                         else {
                             timeout.state = "complete";
+                            // delete it
                             timeout.cancel();
                         }
                     }
@@ -47,11 +71,17 @@ var Scheduler = (function () {
             _this.__INTERNAL__run(_this.interval);
         };
         this.__INTERNAL__run = function (timeout) {
-            if (!_this.shutdown) {
+            if (!_this.shutdown) { // need to check if it has been shutdown before rerunning
                 _this.expectedSchedulerIntervalExpiration = Date.now() + timeout;
                 _this.timeoutId = setTimeout(_this.__INTERNAL__runExpiredCallbacks, timeout);
             }
         };
+        /**
+         * Pauses all jobs and the scheduler itself.
+         *
+         * Optionally supply ignorePauseDelay to override each Timeout's
+         * ignorePauseDelay setting.
+         */
         this.pause = function (ignorePauseDelay) {
             if (_this.remainingPauseTime === undefined) {
                 _this.remainingPauseTime = _this.expectedSchedulerIntervalExpiration - Date.now();
@@ -66,8 +96,17 @@ var Scheduler = (function () {
                 }
             }
         };
+        /**
+         * Resumes the scheduler and all paused timeouts. This does not resume
+         * all paused timeouts if the scheduler itself is not paused. If this
+         * is desired, use {@link getTimeoutsForBucket} and loop through them.
+         *
+         * Optionally supply ignorePauseDelay to override each Timeout's
+         * ignorePauseDelay setting.
+         */
         this.resume = function (ignorePauseDelay) {
             if (_this.remainingPauseTime !== undefined) {
+                // need to restart timeout with lower interval
                 _this.__INTERNAL__run(_this.remainingPauseTime);
                 _this.remainingPauseTime = undefined;
                 for (var bucketsKey in _this.buckets) {
@@ -80,6 +119,12 @@ var Scheduler = (function () {
                 }
             }
         };
+        /**
+         * Adds the callback to be run with the provided options. Defaults will be
+         * set if they are not supplied. options must be a number that will correspond
+         * to a time (in milliseconds) or options must be an object containing 'timeMillis'.
+         * The created Timeout is returned so that it can be controlled as desired.
+         */
         this.add = function (callback, options) {
             if (options === undefined || options === null ||
                 (typeof options !== "number" && options.timeMillis == undefined)) {
@@ -93,6 +138,14 @@ var Scheduler = (function () {
             options.key = options.key || (_this.idCounter++).toString();
             options.bucketKey = options.bucketKey || Scheduler.DEFAULT_BUCKET_KEY;
             var expiration = options.timeMillis + Date.now();
+            /*
+            * Capturing the scheduler's buckets so that it can be used in the cancel
+            * function. It seems better to capture the bucket rather than the key and
+            * bucketKey (and obtain them via options), because the options are more
+            * easily accessed by the user and for whatever reason they could change
+            * them randomly (unlikely, but who knows..). Anyway, one or the other needs
+            * to be captured.
+            * */
             var schedulerBuckets = _this.buckets;
             var timeout = {
                 options: options,
@@ -102,9 +155,13 @@ var Scheduler = (function () {
                     return this.state === "complete";
                 },
                 cancel: function () {
+                    // remove it from bucket's keys
                     if (schedulerBuckets[this.options.bucketKey]) {
                         delete schedulerBuckets[this.options.bucketKey][this.options.key];
                     }
+                    // if (this.buckets[bucketKey]) {
+                    //   delete this.buckets[bucketKey][key];
+                    // }
                 },
                 pause: function (ignorePauseDelay) {
                     if (this.state === 'pending') {
@@ -136,11 +193,17 @@ var Scheduler = (function () {
             }
             var existingTimeoutForKey = bucket[options.key];
             if (existingTimeoutForKey && !options.overwriteKey) {
+                // do not overwrite the existing timeout, return it instead.
                 return existingTimeoutForKey;
             }
+            // does not already exists or should overwrite
             bucket[options.key] = timeout;
             return timeout;
         };
+        /**
+         * Pauses the Timeout if it exists. Returns the Timeout that was paused or
+         * undefined if the Timeout did not exist.
+         */
         this.pauseTimeout = function (timeoutKey, bucketKey, ignorePauseDelay) {
             if (bucketKey === void 0) { bucketKey = Scheduler.DEFAULT_BUCKET_KEY; }
             var bucket = _this.buckets[bucketKey];
@@ -153,9 +216,17 @@ var Scheduler = (function () {
             }
             return;
         };
+        /**
+         * Pauses the Timeout if it exists. Returns the Timeout that was paused or
+         * undefined if the Timeout did not exist.
+         */
         this.pauseTimeoutOnDefaultBucket = function (timeoutKey, ignorePauseDelay) {
             return _this.pauseTimeout(timeoutKey, Scheduler.DEFAULT_BUCKET_KEY, ignorePauseDelay);
         };
+        /**
+         * Resumes the Timeout if it exists. Returns the Timeout that was resumed or
+         * undefined if the Timeout did not exist.
+         */
         this.resumeTimeout = function (timeoutKey, bucketKey, ignorePauseDelay) {
             if (bucketKey === void 0) { bucketKey = Scheduler.DEFAULT_BUCKET_KEY; }
             var bucket = _this.buckets[bucketKey];
@@ -168,9 +239,16 @@ var Scheduler = (function () {
             }
             return;
         };
+        /**
+         * Resumes the Timeout if it exists. Returns the Timeout that was resumed or
+         * undefined if the Timeout did not exist.
+         */
         this.resumeTimeoutOnDefaultBucket = function (timeoutKey, ignorePauseDelay) {
             return _this.resumeTimeout(timeoutKey, Scheduler.DEFAULT_BUCKET_KEY, ignorePauseDelay);
         };
+        /**
+         * Returns all timeouts that have not expired on the given bucket.
+         */
         this.getTimeoutsForBucket = function (bucketKey) {
             var bucket = _this.buckets[bucketKey];
             var timeouts = [];
@@ -182,9 +260,16 @@ var Scheduler = (function () {
             }
             return [];
         };
+        /**
+         * Returns all timeouts that have not expired on the default bucket.
+         */
         this.getDefaultBucketTimeouts = function () {
             return _this.getTimeoutsForBucket(Scheduler.DEFAULT_BUCKET_KEY);
         };
+        /**
+         * Returns the timeout of the given key on the given bucket if it exists
+         * or undefined if it does not exist.
+         */
         this.getTimeoutForKey = function (key, bucketKey) {
             if (bucketKey === void 0) { bucketKey = Scheduler.DEFAULT_BUCKET_KEY; }
             var bucket = _this.buckets[bucketKey];
@@ -193,6 +278,10 @@ var Scheduler = (function () {
             }
             return;
         };
+        /**
+         * Returns the timeout of the given key on the default bucket if it exists
+         * or undefined if it does not exist.
+         */
         this.getTimeoutForDefaultBucket = function (key) {
             return _this.getTimeoutForKey(key);
         };
